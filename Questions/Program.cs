f07315024632576216ac57cd5eddd84d3a20304b
内間 <jaso0125@gmail.com>
2022-09-28 19:29:59 +0900

    競技プログラミング勉強のためのベースとなる標準入力と標準出力の箱を用意

---
+++ Questions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 標準入力
+            var value = Console.ReadLine();
+
+            // 標準出力
+            Console.Write(value);
+        }
+    }
+}
