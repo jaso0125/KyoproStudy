510f81423edfb8ab2ca4210327158f32ad6d36f5
内間 <jaso0125@gmail.com>
2022-09-29 19:50:16 +0900

    B01 - A+B Problem

---
+++ Questions/B01_A+B Problem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    public static partial class B01_A_B_Problem
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- Questions/Program.cs
+++ Questions/Program.cs
@@ -2,15 +2,17 @@
 
 namespace Questions
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // 標準入力
-            var value = Console.ReadLine();
+            var values = Console.ReadLine().Split(' ');
+
+            var result = B01_A_B_Problem.Addition(int.Parse(values[0]), int.Parse(values[1]));
 
             // 標準出力
-            Console.Write(value);
+            Console.Write(result);
         }
     }
 }
