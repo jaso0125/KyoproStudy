4c6e3f687f24e8d5b2b2fb6c1c4b488285edbc25
jaso0125 <jaso0125@gmail.com>
2025-01-20 08:33:44 +0900

    新規作成

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace KyoproStudy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 標準入力
+            var value = Console.ReadLine();
+
+            // 標準出力
+            Console.Write(value);
+        }
+    }
+}
