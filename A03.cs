61288b22b5aaf4d8f399b537eea35666d1c41061
jaso0125 <jaso0125@gmail.com>
2025-01-21 20:04:43 +0900

    A03問題の回答を追加

---
+++ A03.cs
@@ -0,0 +1,25 @@
+﻿namespace KyoproStudy
+{
+    public static partial class A03
+    {
+        public static string TwoCards(string strN, string strK, string[] strArrayP, string[] strArrayQ)
+        {
+            var intN = int.Parse(strN);
+            var intK = int.Parse(strK);
+            var intArrayP = strArrayP.Select(x => int.Parse(x)).ToArray();
+            var intArrayQ = strArrayQ.Select(x => int.Parse(x)).ToArray();
+
+            for (var i = 0; i < intN; i++)
+            {
+                for (var j = 0; j < intN; j++)
+                {
+                    if (intArrayP[i] + intArrayQ[j] == intK)
+                    {
+                        return "Yes";
+                    }
+                }
+            }
+            return "No";
+        }
+    }
+}
