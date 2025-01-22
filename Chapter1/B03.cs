6b8baf33ba72b1219afee1a6ca56cce365be3286
jaso0125 <jaso0125@gmail.com>
2025-01-22 11:37:44 +0900

    B03問題の回答を追加

---
+++ Chapter1/B03.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+namespace KyoproStudy.Chapter1
+{
+    public static partial class B03
+    {
+        public static string Supermarket1(string strN, string[] strArrayA)
+        {
+            var intN = int.Parse(strN);
+            var intArrayA = strArrayA.Select(x => int.Parse(x)).ToArray();
+
+            for (var i = 0; i < intN; i++)
+            {
+                for (var j = i + 1; j < intN; j++)
+                {
+                    for (var k = j + 1; k < intN; k++)
+                    {
+                        if (intArrayA[i] + intArrayA[j] + intArrayA[k] == 1000)
+                        {
+                            return "Yes";
+                        }
+                    }
+                }
+            }
+            return "No";
+        }
+    }
+}
