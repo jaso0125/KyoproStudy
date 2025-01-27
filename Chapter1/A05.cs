982a2acdea1d40ff3d96eeb76552c2cb188f6a72
jaso0125 <jaso0125@gmail.com>
2025-01-27 11:31:24 +0900

    A05問題の回答を追加

---
+++ Chapter1/A05.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyoproStudy.Chapter1
+{
+    public static partial class A05
+    {
+        public static int ThreeCards(string strN, string strK)
+        {
+            var intN = int.Parse(strN);
+            var intK = int.Parse(strK);
+
+            var count = 0;
+
+            for (var i = 1; i < intN + 1; i++)
+            {
+                for (var j = 1; j < intN + 1; j++)
+                {
+                    {
+                        var k = intK - i - j;
+                        if (k >= 1 && k <= intN)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
